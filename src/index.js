c4fa032f7da1e53781bfff56751260fc90c0f994
zenoko <yann.bourgues@gmail.com>
2021-03-17 00:25:14 +0100

    Add navbar

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
 
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
